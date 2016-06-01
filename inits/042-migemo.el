ae2bec965357fb4d0044503f8bb15932c1792996
Yusuke Yoshikawa <y_yoshikawa@atlas-is.co.jp>
2016-06-01 10:37:47 +0900

    ntemacs用のmigemo判定を追加

--- inits/042-migemo.el
+++ inits/042-migemo.el
@@ -4,9 +4,11 @@
 ;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
 
 (use-package migemo
+  :if (and (eq system-type 'windows-nt)
+           (not (eq (executable-find "C:/cmigemo/cmigemo.exe") 'nil)))
   :config
-  (setq migemo-dictionary "C:/cmigemo/dict/utf-8/migemo-dict")
   (setq migemo-command "C:/cmigemo/cmigemo.exe")
+  (setq migemo-dictionary "C:/cmigemo/dict/utf-8/migemo-dict")
   (setq migemo-options '("-q" "--emacs" "-i" "\a"))
   (setq migemo-user-dictionary nil)
   (setq migemo-regex-dictionary nil)
